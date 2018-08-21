f014a9d27d6c23c6c6a9df769ac77e6196a8f2bd
love_bj_lee@163.com <lyz20102471>
2018-08-21 12:01:47 +0800

    ...

---
+++ app/src/main/java/com/lyz/lib/view/map/StationMapView.java
@@ -0,0 +1,28 @@
+package com.lyz.lib.view.map;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by lyz on 2018/8/17.
+ *
+ */
+
+public class StationMapView extends View {
+
+    public StationMapView(Context context) {
+        super(context);
+    }
+
+    public StationMapView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StationMapView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
